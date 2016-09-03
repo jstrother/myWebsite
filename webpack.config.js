ce107b8b88ad7ed778f315ff1d4393bca314b84f
Jim Strother <strotherwebdev@gmail.com>
2016-09-03 23:15:38 +0000

    updated npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
 const path = require('path');
 const webpack = require('webpack');
-
+ 
 module.exports = {
 	entry: './js/scripts.js',
 	output: {
 		path: `public/`,
 		filename: 'scripts.js'
 	},
-	watch: true,
 	devtool: 'source-map',
 	module: {
 		loaders: [{			
